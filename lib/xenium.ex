7999ab8c973caf04737ee4af152d638e2c717d79
Michael Davis <rubiksmaster65@yahoo.com>
2018-01-21 14:42:51 -0500

    Get the ask function running
    I was using the non-bang versions of encode, post, and decode before,
    which return tuples that mark if they're ok or not.

--- lib/xenium.ex
+++ lib/xenium.ex
@@ -4,30 +4,22 @@ defmodule Xenium do
   """
 
   @doc """
-  Ask.
+  Ask!
   
-  Post the XML-RPC server url with a method name and optional parameters
+  Post the XML-RPC server url with a method name and optional parameters.
 
   ## Examples
 
-      iex> Xenium.ask url, "method.name", [:param1, :param2]
+      iex> Xenium.ask! xml-rpc-url, "method.name", [:param1, :param2]
       "response"
 
   """
-  def ask(url, method_name, params \\ []) do
-    #uri = System.get_env("ROS_MASTER_URI")
-#    req = %XMLRPC.MethodCall{method_name: method_name, params: params}
-#          |> XMLRPC.encode
-#    resp = HTTPoison.post(url, req)
-#           |> get_in([:body])
-#           |> XMLRPC.decode
-#           |> get_in([:param])
-
+  def ask!(url, method_name, params \\ []) do
     %XMLRPC.MethodCall{method_name: method_name, params: params}
-    |> XMLRPC.encode
-    |> (&HTTPoison.post(url, &1)).()
+    |> XMLRPC.encode!
+    |> (&HTTPoison.post!(url, &1)).()
     |> Map.get(:body)
-    |> XMLRPC.decode
+    |> XMLRPC.decode!
     |> Map.get(:param)
   end
 
