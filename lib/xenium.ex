c797388e0c3657190482689a7d68af0bf02ba88c
Michael Davis <rubiksmaster65@yahoo.com>
2018-01-21 17:48:45 -0500

    Add a non error throwing version of call
    Returns the common tuple schema of :ok or :error

--- lib/xenium.ex
+++ lib/xenium.ex
@@ -7,6 +7,43 @@ defmodule Xenium do
   """
 
   @doc """
+  Post the XML-RPC server url with a method name and optional parameters.
+
+  Returns a tuple of `{ :ok, response }` or `{ :error, message }`, where the
+  error tuple contains either the error response from HTTPoison or the
+  XMLRPC library.
+  """
+  def call(url, method_name, params \\ []) do
+    # safely pipe the results of each
+    encode(method_name, params)
+    |> (&post(url, &1)).()
+    |> decode
+    |> get_resp
+  end
+
+  defp encode(method_name, params) do
+    %XMLRPC.MethodCall{method_name: method_name, params: params}
+    |> XMLRPC.encode
+  end
+
+  defp post(url, { :ok, body }) do
+    try do
+      HTTPoison.post(url, body)
+    rescue
+      CaseClauseError -> { :error, "HTTPoison error. " <>
+        "Probably an invalid URL." }
+    end
+  end
+  defp post(_url, error), do: error
+
+  defp decode({ :ok, %{ body: body } }), do: XMLRPC.decode(body)
+  defp decode(error), do: error
+
+  defp get_resp({ :ok, error = %XMLRPC.Fault{} }), do: { :error, error }
+  defp get_resp({ :ok, %{ param: resp } }), do: resp
+  defp get_resp(error), do: error
+
+  @doc """
   Post the XML-RPC server url with a method name and optional parameters,
   raising an exception on failure.
 
